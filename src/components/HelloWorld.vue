a8a80fca8bbf8903b17ea63b909ebe79a3ab6946
Ljhhhhhh <13216698987@163.com>
2018-01-25 21:29:40 +0800

    'd'

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <p @click="hide">hello</p>
+    <comp :showValue="showValue" @hideClick="hide" :class="{big1:isbig,small1:issmall}"></comp>
+    <el-button type="success" @click="go" :class="{big:isbig,small:issmall}">BTN</el-button>
+  </div>
+</template>
+<script>
+  import comp from './comp'
+  export default {
+    data(){
+      return {
+        showValue:true,
+        isbig:true,
+        issmall:false
+      }
+    },
+    components:{
+      comp
+    },
+    methods:{
+      hide(){
+        this.isbig=!this.isbig
+        this.issmall=!this.issmall
+      },
+      go(){
+        this.isbig=!this.isbig
+        this.issmall=!this.issmall
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .big{
+    font-size: 40px;
+    color: yellow;
+  }
+  .small{
+    font-size: 12px;
+    color: red;
+  }
+  .big1{
+    width: 500px;
+    height: 500px;
+    line-height: 500px;
+  }
+  .small1{
+    width: 50px;
+    height: 50px;
+    line-height: 50px;
+  }
+</style>
---
+++ src/components/comp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="btn" v-show="showValue" @click="hide">
+    {{value}}
+  </div>
+</template>
+<script>
+  export default {
+    props:['showValue'],
+    data(){
+      return {
+        value:123
+      }
+    },
+    methods:{
+      hide(){
+        this.$emit('hideClick')
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .btn{
+    background-color: aqua;
+    color: #FFF;
+    text-align: center;
+    transition: all .1s;
+  }
+</style>
